150e458cabda0f8d0cf1b871155c8df498aee909
rana sami rezk <104070983+ranasami1@users.noreply.github.com>
2024-01-19 12:34:55 +0200

    change website logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import { BrowserRouter as Router , Routes , Route} from 'react-router-dom';
 import { Home } from './Components/Home/Home';
+import { Menue } from './Components/Menue/Menue';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/menue' element={<Menue/>}/>
       </Routes>
     </Router>
     </div>
---
+++ src/Components/Menue/Menue.js
@@ -0,0 +1,10 @@
+import './Menue.css';
+export function Menue(){
+    return(
+        <div className="menue">
+            <div className="container">
+                <h1>hi</h1>
+            </div>
+        </div>
+    )
+}

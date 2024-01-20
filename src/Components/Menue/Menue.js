36f13f45e007771c8300bc3ab97cac85e08a2d82
rana sami rezk <104070983+ranasami1@users.noreply.github.com>
2024-01-20 19:42:34 +0200

    add menue items

--- src/Components/Menue/Menue.js
+++ src/Components/Menue/Menue.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import './Menue.css';
+import Card from 'react-bootstrap/Card';
 export function Menue(){
+    const [menu,setMenu] = useState([])
+    function menue(){
+            fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken')
+            .then((res)=>res.json())
+            .then((data)=>{
+                setMenu(data.meals)
+            })
+        }
+    
+    useEffect(()=>{
+        menue();
+    },[])
     return(
         <div className="menue">
             <div className="container">
-                <h1>hi</h1>
+                {menu.map((item,key)=>{
+                    return(
+                        <Card key={key} style={{ width: '15rem',margin: "10px"}}>
+                            <Card.Img variant="top" src={item.strMealThumb} />
+                            <Card.Body>
+                                <Card.Title>{item.strMeal}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    )
+                })
+                }
+                
             </div>
         </div>
     )

f58e7e41bb86b0d58251a687e0740d1e49354c66
rana sami rezk <104070983+ranasami1@users.noreply.github.com>
2024-02-07 23:11:26 +0200

    added random section

--- src/Components/randomMeal/random.js
+++ src/Components/randomMeal/random.js
@@ -1,25 +1,33 @@
 import axios from "axios"
 import "./random.css"
 import { useEffect, useState} from "react"
-import { Recipe } from "../recipe/recipe";
 export  function Random(){
     const [isVisible,setIsVisible] = useState(false)
     const [random,setRandom] = useState([]);
     function generateMeal(){
         axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
-        .then((res)=>setRandom(res.data.meals));
+        .then((res)=>setRandom(res.data.meals[0]));
+        console.log(random)
     }
     function hide(){
-        setIsVisible(true);
+        setIsVisible(!isVisible);
     }
     useEffect(()=>{
         generateMeal();
+
     },[])
     return(
         <div className="rondomDish">
             <h1 className="title tracking-in-expand">Today's dish</h1>
             <div className="dish">
-            {isVisible?<Recipe />: <div class="wrap" >
+                
+            {isVisible?
+            <div className="theMeal scale-up-hor-left">
+               <h2 className="text-focus-in">{random.strMeal}</h2>
+               <img  style={{width:"200px",borderRadius:"20%"}}src={random.strMealThumb}/>
+               <a href={random.strYoutube}>Watch The Recipe</a>
+            </div>
+                :<div class="wrap">
                 {random?<button onClick={()=>hide()} id="random">Guess the meal</button>:""}
             </div>}
             </div>
